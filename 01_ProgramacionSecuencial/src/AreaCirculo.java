3dc9c7ebc419b85b7e893964f5afb9ac7b7a5196
Rodolfo <rodo301004@gmail.com>
2025-01-22 11:53:46 -0600

    Logro: Primeros Programas secuencialess

---
+++ 01_ProgramacionSecuencial/src/AreaCirculo.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class AreaCirculo {
+    public static void main(String[] args) {
+
+        //Declaracion de variables
+
+        double radio = .0, area = 0.0;
+        final double PI = 3.1416;
+        String salida = "";
+
+        radio = Double.parseDouble(JOptionPane.showInputDialog("Introduce el radio; "));
+        area = PI + Math.pow(radio,2);
+        salida = "El area del circulo es: " + radio + "es" + area;
+
+        JOptionPane.showMessageDialog();
+    }
+}
---
+++ 01_ProgramacionSecuencial/src/Expresiones.java
@@ -0,0 +1,8 @@
+public class Expresiones {
+    public static void main(String[] args) {
+        double e1 = 2.0+5.0*3.0/2.0+5.0*(2.0+2.0);
+        System.out.println(e1);
+
+        //Declaración de variables
+    }
+}
